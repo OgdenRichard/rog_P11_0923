b845bcbd36a4534a1a534744a5e3b6c24285980f
Richard Ogden <ogden.richard@gmail.com>
2023-09-28 10:43:20 +0200

    28092023 | not found page for housings

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,26 +1,11 @@
-import { useRouteError } from 'react-router-dom';
-import { Navbar } from '../components/Navbar';
-import { Footer } from '../components/Footer';
-
 export function ErrorPage() {
-  // const error = useRouteError();
   return (
     <>
-      {/* <header>
-        <Navbar />
-      </header> */}
       <div id="error-page">
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
-        {/* <p>{error.status}</p> */}
-        <p>
-          erreur
-          {/* <i>{error.statusText || error.message}</i> */}
-        </p>
+        <p>erreur</p>
       </div>
-      {/* <footer>
-        <Footer />
-      </footer> */}
     </>
   );
 }
--- src/pages/HousingDetails.jsx
+++ src/pages/HousingDetails.jsx
@@ -1,8 +1,10 @@
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import housings from '../data/logements.json';
 
 export const HousingDetails = () => {
   const { id } = useParams();
   const details = housings.find((el) => el.id === id);
-  return <div>{details.title}</div>;
+  return (
+    <>{details ? <div>{details.title}</div> : <Navigate to={'/notfound'} />}</>
+  );
 };

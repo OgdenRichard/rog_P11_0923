58a03b3f4791cd86c8c360a2254a63b8aa5d0838
Richard Ogden <ogden.richard@gmail.com>
2023-10-02 09:55:08 +0200

    02102023 | arrow svg

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,10 +1,13 @@
+import arrow from '../assets/images/arrow.svg';
+import { useState } from 'react';
+
 export const Collapse = ({ title }) => {
   return (
     <>
       <div className="collapse">
         <button className="collapse_button">
           {title}
-          <span className="arrow"></span>
+          <img src={arrow} alt="flèche" className="arrow_img"></img>
         </button>
       </div>
     </>

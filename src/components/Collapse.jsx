ab5b3a0a25eac1b9d1b57c4e68187244d77b66f7
Richard Ogden <ogden.richard@gmail.com>
2023-10-02 10:52:48 +0200

    02102023 | toggle tryouts

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,14 +1,20 @@
 import arrow from '../assets/images/arrow.svg';
 import { useState } from 'react';
 
-export const Collapse = ({ title }) => {
+export const Collapse = ({ title, options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = (e) => {
+    setIsOpen(!isOpen);
+    e.currentTarget.classList.toggle('expanded');
+  };
   return (
     <>
       <div className="collapse">
-        <button className="collapse_button">
+        <button className="collapse_button" onClick={toggleDropdown}>
           {title}
           <img src={arrow} alt="flèche" className="arrow_img"></img>
         </button>
+        {isOpen && <div className="dropdown-content show">Content Here</div>}
       </div>
     </>
   );

61b820823350fae5608bd2144cc92c6ba4c7a19f
Richard Ogden <ogden.richard@gmail.com>
2023-10-03 15:51:37 +0200

    03102023 | home > css grid styled

--- src/components/HousingCard.jsx
+++ src/components/HousingCard.jsx
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 export const HousingCard = ({ id, title, cover }) => {
   return (
     <>
-      <figure className="housing__card">
-        <Link to={`${id}`}>
-          <img src={cover} alt="" />
-        </Link>
-      </figure>
+      <Link to={`${id}`}>
+        <figure
+          className="housing__card"
+          style={{
+            gridArea: 'image',
+            backgroundImage: `url(${cover})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
+        >
+          <figcaption>{title}</figcaption>
+        </figure>
+      </Link>
     </>
   );
 };

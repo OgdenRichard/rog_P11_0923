682cc8ccc8a30cf18294024cbed1d717ef3da19e
Richard Ogden <ogden.richard@gmail.com>
2023-10-04 18:14:49 +0200

    04102023 | arrow atom > slider integration

--- src/components/Atoms/Arrow.jsx
+++ src/components/Atoms/Arrow.jsx
@@ -1,6 +1,29 @@
-import arrow from '../assets/images/arrow.svg';
+import arrow from '../../assets/images/arrow.svg';
 
-export const Arrow = ({ direction }) => {
+export const Arrow = ({
+  direction,
+  currentSlide,
+  setCurrent,
+  slidesLength,
+}) => {
+  const changeSlide = (e) => {
+    const forward = e.target.className === 'arrow_right';
+    const last = currentSlide + 1 === slidesLength;
+    if (!currentSlide && !forward) {
+      setCurrent(slidesLength - 1);
+    } else if (forward && last) {
+      setCurrent(0);
+    } else {
+      setCurrent(() => (forward ? currentSlide + 1 : currentSlide - 1));
+    }
+  };
   const altText = direction === 'arrow_right' ? 'suivante' : 'précédente';
-  return <img src={arrow} alt={altText} className={direction} />;
+  return (
+    <img
+      src={arrow}
+      alt={altText}
+      className={direction}
+      onClick={changeSlide}
+    />
+  );
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,8 @@
-import arrow from '../assets/images/arrow.svg';
 import { useState } from 'react';
+import { Arrow } from './Atoms/Arrow';
 
 export const Slider = ({ slides }) => {
   const [currentSlide, setCurrent] = useState(0);
-  const changeSlide = (e) => {
-    const forward = e.target.className === 'arrow_right';
-    const last = currentSlide + 1 === slides.length;
-    if (!currentSlide && !forward) {
-      setCurrent(slides.length - 1);
-    } else if (forward && last) {
-      setCurrent(0);
-    } else {
-      setCurrent(() => (forward ? currentSlide + 1 : currentSlide - 1));
-    }
-  };
   const isSlide = slides.length > 1;
   return (
     <div
@@ -28,17 +17,17 @@ export const Slider = ({ slides }) => {
     >
       {isSlide && (
         <>
-          <img
-            src={arrow}
-            alt="précédente"
-            className="arrow_left"
-            onClick={changeSlide}
+          <Arrow
+            direction={'arrow_left'}
+            currentSlide={currentSlide}
+            setCurrent={setCurrent}
+            slidesLength={slides.length}
           />
-          <img
-            src={arrow}
-            alt="suivante"
-            className="arrow_right"
-            onClick={changeSlide}
+          <Arrow
+            direction={'arrow_right'}
+            currentSlide={currentSlide}
+            setCurrent={setCurrent}
+            slidesLength={slides.length}
           />
         </>
       )}

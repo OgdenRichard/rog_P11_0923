8f5ae68b1165e88bfa33761af93a23989299e830
Richard Ogden <ogden.richard@gmail.com>
2023-10-05 17:47:32 +0200

    05102023 | housing detail & tags

--- src/pages/HousingDetails.jsx
+++ src/pages/HousingDetails.jsx
@@ -1,16 +1,27 @@
 import { Navigate, useParams } from 'react-router-dom';
 import housings from '../data/logements.json';
 import { Slider } from '../components/Slider';
+import { Tag } from '../components/Atoms/Tag';
 
 export const HousingDetails = () => {
   const { id } = useParams();
   const details = housings.find((el) => el.id === id);
-  const slides = details ? details.pictures : [];
   return (
     <>
       <main>
-        {slides.length ? (
-          <Slider slides={slides} />
+        {details ? (
+          <>
+            <Slider slides={details.pictures} />
+            <div className="details_wrapper">
+              <h1>{details.title}</h1>
+              <p>{details.location}</p>
+              <div className="tags_container">
+                {details.tags.map((tag, index) => (
+                  <Tag key={index} tag={tag} />
+                ))}
+              </div>
+            </div>
+          </>
         ) : (
           <Navigate to={'notfound'} />
         )}

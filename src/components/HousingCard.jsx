89e349fb6296bfbbec835adf0e29330cca11c08a
Richard Ogden <ogden.richard@gmail.com>
2023-10-11 16:08:29 +0200

    11102023 | HousingCard > default image

--- src/components/HousingCard.jsx
+++ src/components/HousingCard.jsx
@@ -1,26 +1,29 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import default_house from '../assets/images/default_house.jpg';
 
 export const HousingCard = ({ id, title, cover }) => {
   return (
     <>
-      <Link to={`${id}`}>
-        <figure
-          className="housing__card"
-          style={{
-            gridArea: 'image',
-            backgroundImage: `linear-gradient(
+      {id.length && (
+        <Link to={`${id}`}>
+          <figure
+            className="housing__card"
+            style={{
+              gridArea: 'image',
+              backgroundImage: `linear-gradient(
               rgba(0, 0, 0, 0.15), 
               rgba(0, 0, 0, 0.15)
             ),url(${cover})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-          }}
-        >
-          <figcaption>{title}</figcaption>
-        </figure>
-      </Link>
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+            }}
+          >
+            <figcaption>{title}</figcaption>
+          </figure>
+        </Link>
+      )}
     </>
   );
 };
@@ -29,4 +32,8 @@ HousingCard.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string,
   cover: PropTypes.string,
+};
+
+HousingCard.defaultProps = {
+  cover: default_house,
 };
